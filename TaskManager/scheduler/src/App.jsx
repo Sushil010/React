68151a3385b743fd81c000216d96da985163c323
sushil sharma <sharma.s420823@gmail.com>
2024-08-30 22:23:17 +0545

    -

---
+++ TaskManager/scheduler/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='relative w-full h-screen bg-zinc-800'>
+      <div className=' absolute top-[50%] left-1/2 -translate-x-[50%] -translate-y-[50%]  text-[12vw] leading-none tracking-tighter text-zinc-900'>Docs</div>
+    </div>
+  )
+}
+
+export default App
