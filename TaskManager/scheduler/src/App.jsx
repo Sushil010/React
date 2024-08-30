9ad1bf9873cdd5a125e90cb17bd439bcea3acb70
sushil sharma <sharma.s420823@gmail.com>
2024-08-30 22:59:32 +0545

    -

--- TaskManager/scheduler/src/App.jsx
+++ TaskManager/scheduler/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Background from './components/Background'
 
 function App() {
   return (
     <div className='relative w-full h-screen bg-zinc-800'>
-      <div className=' absolute top-[50%] left-1/2 -translate-x-[50%] -translate-y-[50%]  text-[12vw] leading-none tracking-tighter text-zinc-900'>Docs</div>
+
+    <Background/>
     </div>
   )
 }
