23e5e247e5ed36f512363a602c7d682efccb0ebf
sushil sharma <sharma.s420823@gmail.com>
2024-08-31 13:32:44 +0545

    foreground

--- TaskManager/scheduler/src/components/Background.jsx
+++ TaskManager/scheduler/src/components/Background.jsx
@@ -3,11 +3,15 @@ import React from 'react'
 function Background() {
   return (
     <>
+    <div className='fixed z-[2] w-full h-screen'>
+
     <div className='absolute flex w-full justify-center top-[2%] py-5 font-semibold text-xl'>Navbar</div>
     <div className=' absolute top-[50%] left-1/2 -translate-x-[50%] -translate-y-[50%]  text-[12vw] leading-none tracking-tighter text-zinc-900 font-semibold'>Docs.</div>
+
+
+    </div>
     
     </>
-    
   )
 }
 
