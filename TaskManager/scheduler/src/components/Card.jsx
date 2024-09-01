2ec8dc8820452ea4feecf50b8da08ca38f1e8f98
sushil sharma <sharma.s420823@gmail.com>
2024-09-01 10:55:35 +0545

    added card

--- TaskManager/scheduler/src/components/Card.jsx
+++ TaskManager/scheduler/src/components/Card.jsx
@@ -5,22 +5,26 @@ import { MdOutlineFileDownload } from "react-icons/md";
 
 function Card() {
   return (
-    <div className='relative w-60 h-72 rounded-[40px] bg-red-800 p-5 text-white overflow-hidden'>
+    <div className='relative w-60 h-72 rounded-[40px] bg-zinc-700 p-5 text-white overflow-hidden'>
       <FaFileAlt/>
     
       <p className='mt-5 text-xm'>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
     
     
-      <div className='absolute footer bottom-0 w-full py-3 px-8 left-0'>
+      <div className='absolute footer bottom-0 w-full  left-0'>
       
-        <div className='flex items-center justify-between mb-5'>
+        <div className='flex items-center justify-between py-3 px-8 mb-5'>
           <h5>0.4mb</h5>
           <span className='w-7 h-7 rounded-full bg-zinc-600 flex items-center justify-center'>
-            <MdOutlineFileDownload size='1.2em' color='#fff'/>
+            <MdOutlineFileDownload size='1em' color='#fff'/>
           </span>
-           
+
         </div>
-      
+
+        <div className='tag bg-green-600 w-full h-7'>
+          <h3 className='text-black justify-center items-center flex font-semibold text-sm '>Download Now</h3>
+        </div>
+                 
 
 
 
