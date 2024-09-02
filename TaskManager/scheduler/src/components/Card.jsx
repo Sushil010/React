5ae73dc5de1a33b1bf78492779f8c4556a725ce1
sushil sharma <sharma.s420823@gmail.com>
2024-09-02 16:52:35 +0545

    -

--- TaskManager/scheduler/src/components/Card.jsx
+++ TaskManager/scheduler/src/components/Card.jsx
@@ -1,29 +1,37 @@
 import React from 'react';
 import { FaFileAlt } from "react-icons/fa";
 import { MdOutlineFileDownload } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
 
 
-function Card() {
+function Card({dataed}) {
   return (
     <div className='relative w-60 h-72 rounded-[40px] bg-zinc-700 p-5 text-white overflow-hidden'>
       <FaFileAlt/>
     
-      <p className='mt-5 text-xm'>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+      <p className='mt-5 text-xm'>{dataed.desc}</p>
     
     
       <div className='absolute footer bottom-0 w-full  left-0'>
       
         <div className='flex items-center justify-between py-3 px-8 mb-5'>
-          <h5>0.4mb</h5>
+          <h5>{dataed.filesize}</h5>
+          
           <span className='w-7 h-7 rounded-full bg-zinc-600 flex items-center justify-center'>
-            <MdOutlineFileDownload size='1em' color='#fff'/>
+
+            {dataed.close ? <IoClose/>:<MdOutlineFileDownload size='1em' color='#fff'/>}
+            
           </span>
 
         </div>
 
-        <div className='tag bg-green-600 w-full h-10'>
-          <h3 className='text-black justify-center items-center flex font-semibold text-sm py-2 '>Download Now</h3>
-        </div>
+        {dataed.tag.isOpen && (
+            <div className='tag bg-green-600 w-full h-10'>
+              <h3 className='text-black justify-center items-center flex font-semibold text-sm py-2 '>{dataed.tag.tagTitle}</h3>
+            </div>
+        )}
+
+        
                  
 
 
--- TaskManager/scheduler/src/components/Foreground.jsx
+++ TaskManager/scheduler/src/components/Foreground.jsx
@@ -2,10 +2,62 @@ import React from 'react'
 import Card from './card'
 
 function Foreground() {
-  return (
-    <div className='fixed top-0 left-0 z-[3] w-full h-full'>
+  
+  const data=[
+    {
+      desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize:".9mb",
+      close:true,
+      tag:{
+        isOpen:true,
+        tagTitle:"Download Now",
+        tagColor:"green"
+      }
+    },
 
-    <Card/>
+
+    {
+      desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize:".9mb",
+      close:true,
+      tag:{
+        isOpen:true,
+        tagTitle:"Download Now",
+        tagColor:"green"
+      }
+    },
+
+
+
+    {
+      desc:"Lorem ipsum dolor sit amet consectetur adipisicing.",
+      filesize:".9mb",
+      close:true,
+      tag:{
+        isOpen:true,
+        tagTitle:"Download Now",
+        tagColor:"green"
+      }
+    },
+]
+
+
+
+  
+  
+  
+  
+  
+  
+  return (
+    <div className='fixed top-0 left-0 z-[3] w-full h-full flex gap-[3rem] p-4 flex-wrap'>
+
+    
+
+
+    {data.map((item,index)=>(
+        <Card dataed={item}/>
+    ))}
 
         
     </div>
