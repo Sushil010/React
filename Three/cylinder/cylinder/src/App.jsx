f21bad7e291eb3143893e1a71e5b6c90e066d5d5
sushil sharma <sharma.s420823@gmail.com>
2024-09-06 23:44:37 +0545

    -

--- Three/cylinder/cylinder/src/App.jsx
+++ Three/cylinder/cylinder/src/App.jsx
@@ -1,9 +1,22 @@
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
+
+import Cyl from './Cyl'
+import "./style.css"
+
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Canvas>
+      <OrbitControls/>
+      <ambientLight/>
+      <Cyl/>
+    </Canvas>
   )
 }
+
 
 export default App
