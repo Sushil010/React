9d28609037e43bb6b38bcdb6daba19d02aaa3a82
sushil sharma <sharma.s420823@gmail.com>
2024-09-06 20:30:23 +0545

    -

---
+++ Frontend/frontend/src/components/Landing.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div className='Landing w-full h-screen bg-zinc-900 px-10 pt-2'>
+
+    <div className='outer'>
+        <div className='texter font-["Fo"] text-[100px] uppercase mt-[130px]'>
+            <h1>We create</h1>
+
+        </div>
+
+    </div>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Landing
