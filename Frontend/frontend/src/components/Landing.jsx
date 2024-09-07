dfef8dc5c606554965937b0c771219938d43d0ed
sushil sharma <sharma.s420823@gmail.com>
2024-09-07 21:17:16 +0545

    -

--- Frontend/frontend/src/components/Landing.jsx
+++ Frontend/frontend/src/components/Landing.jsx
@@ -19,15 +19,25 @@ function Landing() {
     <div className='border-t-2  border-zinc-700 mt-[17vh]'> 
     </div>
 
-    <div className='px-10 py-5  mt-[15px] flex justify-between align-center'>
+  <div className='px-10 py-5  mt-[5px] flex        justify-between align-center'>
     {['For public and private companies','From the first pitch to IPO'].map((items,index)=>(
       <p className={`text-base tracking-tight leading-none  `}>{items}</p>
     ))
     }
 
-    <div className=' px-3 py-2 -mt-[10px] button uppercase border-[1] bg-zinc-950 rounded-full'>start the project</div>
 
-    <div className=''>MdArrowOutward</div>
+  <div className='flex justify-center items-center gap-1'>
+    <div className=' px-3 py-2  button uppercase bg-zinc-950 border-[.5px] rounded-full'>start the project
+
+    </div>
+
+    <div className='w-11 h-11 ml-[2px] rounded-full bg-zinc-950 border-[.5px] flex items-center justify-center'>
+      <MdArrowOutward/>
+    </div>
+  </div>
+    
+
+    
 
 
     </div>
