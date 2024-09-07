e5b411e529228398352e0ca3b558e908ab65574c
sushil sharma <sharma.s420823@gmail.com>
2024-09-07 16:10:17 +0545

    -

--- Frontend/frontend/src/components/Landing.jsx
+++ Frontend/frontend/src/components/Landing.jsx
@@ -4,12 +4,13 @@ function Landing() {
   return (
     <div className='Landing w-full h-screen bg-zinc-900 px-10 pt-2'>
 
-    <div className='outer'>
-        <div className='texter font-["Fo"] text-[100px] uppercase mt-[130px] leading-tight tracking-tighter'>
-            <h1>We create</h1>
-
-        </div>
-
+    <div className='outer mt-[25vh]'>
+      {["We create","eye-opening","presentations"].map((item,index)=>{
+          return <div className='texter text-[100px] uppercase leading-[5.5vw] tracking-tighter font-medium'>
+          <h1>{item}</h1>
+      </div>
+      })
+      }
     </div>
 
 
