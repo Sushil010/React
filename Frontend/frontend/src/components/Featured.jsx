6dd6425a3113675d1a52390237ad4ad78a37c8e6
sushil sharma <sharma.s420823@gmail.com>
2024-09-11 16:51:21 +0545

    -

--- Frontend/frontend/src/components/Featured.jsx
+++ Frontend/frontend/src/components/Featured.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { GoDotFill } from "react-icons/go";
+
 
 function Featured() {
   return (
-    <div className=' w-full  text-black bg-red-800 '>
+    <div className=' w-full  text-white'>
         <div className=' text-[3.5vw] py-24 tracking-tight'>
                 <h1 className='px-10'>Featured projects</h1>
                 <div className=' border-b-2 border-zinc-600 mt-6'>
@@ -10,28 +12,104 @@ function Featured() {
                 </div>
         </div>
 
+        <div className=' px-10 relative w-full h-screen  -my-20 '>
 
-        <div className=' flex w-full h-screen bg-orange-600'>
+          <div className='flex gap-[41vw] uppercase'>
+          <div className='flex justify-center mt-20 items-center '>
+              <div><GoDotFill/></div>
+              <h3>cardboard</h3>
+          </div>
+         <div className='flex mt-20 justify-center items-center' >
+              <div><GoDotFill/></div>
+              <h3>ah2 and matt horn</h3>
+         </div>
+         </div>
 
-            <div className=' px-10 left w-1/2 h-full bg-gray-700'>
-                <h3 className='uppercase '> cardboard spaceship</h3>
+          <div className='flex absolute gap-5 w-[97%] h-[80%] '>
 
-                <div className='h-[40.9vw] w-[50vw] bg-[url("https://ochi.design/wp-content/uploads/2024/08/CS_Website_1-663x551.png")] bg-cover bg-no-repeat mt-5 rounded-3xl'>
+          
 
-                </div>
+            <div className='left w-1/2 h-full  rounded-2xl  bg-[url("https://ochi.design/wp-content/uploads/2024/08/CS_Website_1-663x551.png")] bg-contain'>
+
+              
 
             </div>
 
-            <div className='right'>
-            <h3 className='uppercase '> cardboard spaceship</h3>
-
-                <div className='h-[40.9vw] w-[50vw] bg-[url("https://ochi.design/wp-content/uploads/2024/08/CS_Website_1-663x551.png")] bg-cover bg-no-repeat mt-5 rounded-3xl'>
-
-                </div>
+            <div className='right w-1/2 h-full  rounded-2xl  bg-[url("https://ochi.design/wp-content/uploads/2024/08/Frame-481692-1-663x551.png")] bg-contain '>
 
             </div>
+
+          </div>
 
         </div>
+
+
+
+        <div className=' px-10 relative w-full h-screen  -my-20 '>
+
+          <div className='flex gap-[41vw] uppercase'>
+          <div className='flex justify-center mt-20 items-center '>
+              <div><GoDotFill/></div>
+              <h3>cardboard</h3>
+          </div>
+         <div className='flex mt-20 justify-center items-center' >
+              <div><GoDotFill/></div>
+              <h3>ah2 and matt horn</h3>
+         </div>
+         </div>
+
+          <div className='flex absolute gap-5 w-[97%] h-[80%] '>
+
+          
+
+            <div className='left w-1/2 h-full  rounded-2xl  bg-[url("https://ochi.design/wp-content/uploads/2024/08/CS_Website_1-663x551.png")] bg-contain'>
+
+              
+
+            </div>
+
+            <div className='right w-1/2 h-full  rounded-2xl  bg-[url("https://ochi.design/wp-content/uploads/2024/08/Frame-481692-1-663x551.png")] bg-contain '>
+
+            </div>
+
+          </div>
+
+        </div>
+
+
+        <div className=' px-10 relative w-full h-screen  -my-20 '>
+
+          <div className='flex gap-[41vw] uppercase'>
+          <div className='flex justify-center mt-20 items-center '>
+              <div><GoDotFill/></div>
+              <h3>cardboard</h3>
+          </div>
+         <div className='flex mt-20 justify-center items-center' >
+              <div><GoDotFill/></div>
+              <h3>ah2 and matt horn</h3>
+         </div>
+         </div>
+
+          <div className='flex absolute gap-5 w-[97%] h-[80%] '>
+
+          
+
+            <div className='left w-1/2 h-full  rounded-2xl  bg-[url("https://ochi.design/wp-content/uploads/2024/08/CS_Website_1-663x551.png")] bg-contain'>
+
+              
+
+            </div>
+
+            <div className='right w-1/2 h-full  rounded-2xl  bg-[url("https://ochi.design/wp-content/uploads/2024/08/Frame-481692-1-663x551.png")] bg-contain '>
+
+            </div>
+
+          </div>
+
+        </div>
+
+
+        
 
         
 
