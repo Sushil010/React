4ddadce62af1882fc206453d784b89e3e21d1e40
sushil sharma <sharma.s420823@gmail.com>
2024-11-24 22:48:16 +0545

    -

--- CustomReact/reactscript.js
+++ CustomReact/reactscript.js
@@ -1,10 +1,21 @@
 function visibleOnPage(reactElement,element) {
-    const domElement=document.createElement(reactElement.type)
+    // const domElement=document.createElement(reactElement.type)
+    // domElement.innerHTML=reactElement.children
+    // domElement.setAttribute('href',reactElement.props.href)
+    // domElement.setAttribute('target',reactElement.props.target)
+
+    // element.appendChild(domElement)
+
+
+    const domElement = document.createElement(reactElement.type)
     domElement.innerHTML=reactElement.children
-    domElement.setAttribute('href',reactElement.props.href)
-    domElement.setAttribute('target',reactElement.props.target)
+
+    for(const prop in reactElement.props){
+        domElement.setAttribute(prop,reactElement.props[prop])
+    }
 
     element.appendChild(domElement)
+
 }
 
 
