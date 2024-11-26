3cca67c657b0053112b530c7aa5c06614b95482d
Sushil Sharma <sharma.s420823@gmail.com>
2024-11-26 10:54:35 +0545

    -

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -4,30 +4,62 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // let counter=0
+  // the array index 0 is making a variable named counter with initial value of 0 as in the useState
+  //setCoubter is method for controlling counter variable 
+  let [counter, setcounter] = useState(0)
+
+
+  const addvalue =()=>{
+    if (counter<3) {
+    counter=counter+1
+    setcounter(counter)
+    }
+    // else{
+    //   alert("Cannot go above 3")
+    // }
+    
+  }
+
+  const decreaseValue=()=>{
+    if (counter>0) {
+      counter=counter-1
+    setcounter(counter)
+    }
+    //  else {
+    //   alert("Cannot go below 0")
+    // }
+    
+    
+  }
+
+
+  // const addvalue = () => {
+  //   value=value+1
+  //   console.log("Added value", value)
+  // }
+
+  // const decreaseValue=()=>{
+  //   value=value-1
+  //   console.log("Decreased value",value)
+  // }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>hello</h1>
+     <h2>Counter : {counter}</h2>
+
+    
+    {counter>=3 && <h1>Cannot go beyond this</h1>}
+
+     <button
+     onClick={addvalue } >Add Value</button>
+    <br/>
+     <button
+     onClick={decreaseValue}
+     >Remove value</button>
+
+     {counter<=0 && <h1>Cannot go below this value</h1>}
     </>
   )
 }
