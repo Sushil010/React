c29e6e8c4ad99dad008c0e8a374e7b8574f17644
Sushil Sharma <sharma.s420823@gmail.com>
2024-11-28 18:27:35 +0545

    -

---
+++ currency-converter/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-white bg-slate-700 w-screen h-screen text-3xl' >
+      
+      
+      App
+      
+      </div>
+  )
+}
+
+export default App
