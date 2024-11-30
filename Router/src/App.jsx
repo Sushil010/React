fce8b4155d206956b0a974a922bca9352029243d
Sushil Sharma <sharma.s420823@gmail.com>
2024-11-30 20:31:57 +0545

    -

---
+++ Router/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-slate-700 w-screen h-screen text-white text-4xl'>
+      
+      App
+
+    </div>
+  )
+}
+
+export default App
