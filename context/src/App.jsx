c019ee4d8e49709084974ebc02e7e11d14d7c156
Sushil Sharma <sharma.s420823@gmail.com>
2024-12-02 14:55:33 +0545

    -

---
+++ context/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-screen h-screen bg-slate-600 text-3xl text-white'>
+      
+      App
+      
+      </div>
+  )
+}
+
+export default App
