67196c9b5aa3483665054a73363954d185b033a6
Sushil Sharma <sharma.s420823@gmail.com>
2024-12-03 16:58:02 +0545

    -

---
+++ themeswitch/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+
+    <div className='w-screen h-screen bg-blue-600 text-2xl'>
+    
+    <div>
+      App
+       
+    </div>
+    
+    </div>
+      </>
+  )
+}
+
+export default App
