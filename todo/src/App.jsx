a51b28c59b16a903bd4fce8c9da3ed7ff55cb9d9
Sushil Sharma <sharma.s420823@gmail.com>
2024-12-04 16:44:34 +0545

    -

---
+++ todo/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    
+    <>
+    
+    <div className='w-screen h-screen bg-zinc-700 text-white text-2xl'>
+      
+      App
+      
+      </div>
+
+      </>
+  )
+}
+
+export default App
