e573ddbc221f6148319894c6736542e43116a1c1
Sushil Sharma <sharma.s420823@gmail.com>
2024-12-06 20:58:00 +0545

    -

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
 const App = () => {
+
+
+  console.log(import.meta.env.VITE_APPWRITE_URL)
   return (
     <div className='h-screen w-screen bg-blue-500 text-white text-2xl' >
       
-      App
+      
+      
       
       </div>
   )
