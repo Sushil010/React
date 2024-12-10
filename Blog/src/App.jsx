29ff85746d252a8dee4258005e4a1b6be4cccf1d
Sushil Sharma <sharma.s420823@gmail.com>
2024-12-10 18:40:57 +0545

    -

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -1,17 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
+import authservice from "./appwrite/auth"
+import {login,logout} from "./store/authSlice"
+import {Header,Footer} from './components/index'
+
 
 const App = () => {
 
+  const [loading, setloading] = useState(true)
 
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <div className='h-screen w-screen bg-blue-500 text-white text-2xl' >
-      
-      
-      
-      
+  const dispatch= useDispatch()
+
+
+  //Ass application will load
+  //ask the applcation if user is logged in or not
+  useEffect(() => {
+    
+    authservice.getCurrentUser()
+    .then((userData)=>
+      {
+        if (userData) {
+          dispatch(login({userData}))
+        }
+        else{
+          dispatch(logout())
+        }
+      })
+    
+      .finally(()=>{setloading(false)})
+
+  }, [])
+  
+
+ return !loading ? 
+
+
+  <div className='w-screen h-screen bg-slate-700 text-white text-2xl'>
+    <Header /> 
+      <div class="flex-col gap-4 w-full flex items-center justify-center">
+        <div
+        class="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full"
+        >
+        <div
+          class="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"
+        >   
+        </div>
+        </div>
       </div>
-  )
-}
 
+    <Footer/>
+  </div>
+ 
+ 
+
+  :
+  null
+}
 export default App
