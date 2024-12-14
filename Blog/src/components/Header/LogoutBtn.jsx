47c5ff098f4b693c1ad0292a8eec353dec350d8a
Sushil Sharma <sharma.s420823@gmail.com>
2024-12-14 16:38:11 +0545

    nav_change

--- Blog/src/components/Header/LogoutBtn.jsx
+++ Blog/src/components/Header/LogoutBtn.jsx
@@ -4,18 +4,30 @@ import {useDispatch} from 'react-redux'
 import authservice from '../../appwrite/auth'
 import {logout} from '../../store/authSlice'
 
+
+
+
+
+
 const LogoutBtn = () => {
 
 
     const dispatch = useDispatch()
     const logoutHandler=()=>{
+        // Calls the Logout method from authservice to delete the
+        //  user's session on the server (returns a promise).
         authservice.Logout.then(()=>
             dispatch(logout())
+            // Once the promise resolves, dispatches the logout action
+            //  from authSlice to update the Redux state (status set to false, userData cleared).
         )
     }
 
   return (
-    <div>LogoutBtn</div>
+    <button
+    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+    onClick={logoutHandler}
+    >Logout</button>
   )
 }
 
