8393ed352b2329203fdc556ebd772aeba0022951
Sushil Sharma <sharma.s420823@gmail.com>
2024-12-16 13:22:30 +0545

    input-field

--- Blog/src/components/Input.jsx
+++ Blog/src/components/Input.jsx
@@ -11,14 +11,26 @@ import React,{useId} from 'react'
  {
     const id=useId()
     
-    return label && <label 
-    htmlFor="{id}"
+    return 
+    (
+   <div className='w-full'> 
+    
+    {label && <label 
+    htmlFor={id}
     className='inline-block mb-1 pl-1'
     >
-
         {label}
-        
-    </label>
+    </label>}
+
+    <input 
+    type={type}
+    className={`px-3 py-2 rounded-lg bg-white text-black outline-none
+        focus:bg-gray-50 duration-200 border border-gray-200 w-full
+        ${className}`}
+    />
+    </div> 
+ 
+    )
  })
 
 export default Input
