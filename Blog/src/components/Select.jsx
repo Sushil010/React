65488ec6b2801d6a7b210054c5ef546ef91c862b
Sushil Sharma <sharma.s420823@gmail.com>
2024-12-17 18:38:24 +0545

    dropdown

--- Blog/src/components/Select.jsx
+++ Blog/src/components/Select.jsx
@@ -12,8 +12,30 @@ function Select({
     const id = useId()
     
   return (
-    <div>Select</div>
+    <div>
+        {label && <label htmlFor={id} className=''> </label>}
+
+        <select 
+        {...props}
+        id={id}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none
+             focus:bg-gray-50 duration-200 border border-gray-200 w-full
+            ${className}`}
+        ref={ref}
+        >
+            {options?.map((option)=>(
+                <option key={option} value="option">
+                    {option}
+                </option>
+            )
+            )}
+
+        </select>
+
+
+
+    </div>
   )
 }
 
-export default Select
+export default React.forwardRef(Select)
