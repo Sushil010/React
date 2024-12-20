d26e838cad41d11f179bb5d66be6657fbe095986
Sushil Sharma <sharma.s420823@gmail.com>
2024-12-20 22:23:01 +0545

    Authlayout_setup

---
+++ Blog/src/components/AuthLayout.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import {useSelector} from 'react-redux'
+
+export default function Protected() {
+  return (
+    <div>AuthLayout</div>
+  )
+}
+
