f202b8fe186774ad7ae96f7cf8227b87c9eda358
Sushil Sharma <sharma.s420823@gmail.com>
2024-12-20 23:00:48 +0545

    authlayout_logic

--- Blog/src/components/AuthLayout.jsx
+++ Blog/src/components/AuthLayout.jsx
@@ -12,7 +12,7 @@ export default function Protected({children,authentication=true}) {
       if (authentication && authStatus !== authentication) {
         navigate("/login")
       }
-      else if(authentication && authStatus == authentication)
+      else if(!authentication && authStatus !== authentication)
       {
         navigate("/")
       }
