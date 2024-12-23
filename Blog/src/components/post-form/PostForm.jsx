55dbe4b673572ebbb630af9d00abb4ce19971cc5
Sushil Sharma <sharma.s420823@gmail.com>
2024-12-23 19:04:40 +0545

    default_setup

--- Blog/src/components/post-form/PostForm.jsx
+++ Blog/src/components/post-form/PostForm.jsx
@@ -1,10 +1,30 @@
 import React, { useCallback } from 'react'
 import {Button, Input, RTE,Select} from '../index'
 import { useForm } from 'react-hook-form'
+import { service } from '../../appwrite/config'
+import { useNavigate} from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+function PostForm({post}) {
+
+
+    const {register,handleSubmit,watch,control,getValues,setValue}=
+    useForm({
+        defaultValues:{
+            //use of ternary operator
+            //title: post.title ? post.title : " "
+            //logival or is used just to reduce verbosity
+            //access the title property of post only if post is not null
+
+            title:post?.title || "",
+            content:post?.content || "",
+            status:post?.status||"",
+            slug:post?.slug || "",
+        }
+    })
 
 
 
-function PostForm() {
   return (
     <div>PostForm</div>
   )
