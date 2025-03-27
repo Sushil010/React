e59c9eacb4b1302ed0c962a3d504279254545705
Sushil Sharma <sharma.s420823@gmail.com>
2025-03-27 20:29:38 +0545

    setUpdated_

--- v1/src/App.jsx
+++ v1/src/App.jsx
@@ -4,19 +4,36 @@ const App = () => {
    
 
   const [initial, setInitial] = useState(0)
+  const [background, setbackground] = useState("green")
   
+
   const counter=()=>{
     
     setInitial(initial+1)
   }
 
+  const decrement=()=>{
+    setInitial(initial-1)
+  }
+
   return (
 
-    <div>
+    <div style={{background: "background"}}>
       
      <h4>Value of a is {initial}</h4>
      <button onClick={counter}>
       Increase value!!
+     </button>
+     <button onClick={decrement}>
+      Decrease value!!
+     </button>
+     <button onClick={
+      ()=>
+      setbackground("red")
+      }>
+        
+        change color!!
+
      </button>
       
     </div>
