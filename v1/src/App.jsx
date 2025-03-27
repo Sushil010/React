70264b32631c305322b765a80a0e741f6fd1ebde
Sushil Sharma <sharma.s420823@gmail.com>
2025-03-27 21:05:39 +0545

    toggle_updater

--- v1/src/App.jsx
+++ v1/src/App.jsx
@@ -45,3 +45,43 @@
 // export default App
 
 
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [lighter, setlighter] = useState("yellow")
+  const [icon, setIcon] = useState("light💡")
+  
+  const light=()=>{
+    setlighter(prev=>(prev==="yellow"?"black":"yellow"))
+  }
+
+  const changeIcon=()=>{
+    setIcon(prev=>(prev==="light💡"?"dark⚫":"light💡"))
+  }
+
+  const allUpdater=()=>{
+    light();
+    changeIcon();
+  }
+
+
+  return (
+    
+    
+    <div style={{background:lighter,width:"full",height:"100vh"}}>
+
+    <button onClick={allUpdater} >
+      {icon} mode
+    </button>
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default App
