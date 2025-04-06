835982c1c195917b8829121b6a61623f9e43e1f7
Sushil Sharma <sharma.s420823@gmail.com>
2025-04-06 18:44:53 +0545

    link_setup

--- v1/src/App.jsx
+++ v1/src/App.jsx
@@ -504,14 +504,29 @@
 
 
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { Home } from "../pages/Home";
+import { About } from "../pages/About";
+import { Contact } from "../pages/Contact";
+import { Products } from "../pages/Products";
 
 const App = () => {
   return (
-    <div>
+    <div className='bg-black text-white w-full h-[100vh]'>
       
       
-      App
+      <Routes>
 
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/products' element={<Products/>}/>
+
+
+
+      </Routes>
+      
+    
     </div>
   )
 }
