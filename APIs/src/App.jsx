89572fdf09b4b9352a4ef440fd7b63e22898fd87
Sushil Sharma <sharma.s420823@gmail.com>
2025-06-06 18:01:53 +0545

    img-1

--- APIs/src/App.jsx
+++ APIs/src/App.jsx
@@ -37,9 +37,9 @@ const App = () => {
     <div className='w-full h-full bg-gray-200'>
       <div className='px-4 py-5'>
 
-        <button onClick={fetcher} className='bg-green-500 active:scale-90 border cursor-pointer border-amber-400 p-2 '>
+        {/* <button onClick={fetcher} className='bg-green-500 active:scale-90 border cursor-pointer border-amber-400 p-2 '>
           Get Data
-        </button>
+        </button> */}
 
         {/* {data.map(function(values){
           return <h1>{values.date}</h1>
@@ -47,15 +47,15 @@ const App = () => {
 
 
         {loader?
-        (<div>
+        (<div className='flex justify-center items-center'>
           <CircularProgress color="success"/>
         </div>)
         
         :
         <>
-        <h1>{data.title}</h1>
-        <img src={data.url} alt="" style={{maxWidth:"100%"}}/>
-        <h1>{data.explanation}</h1>
+        <h1 className='text-blue-800'>{data.title}</h1>
+        <img src={data.url} alt="" style={{maxWidth:"50%"}}/>
+        {/* <h1>{data.explanation}</h1> */}
         </>
         
         }
