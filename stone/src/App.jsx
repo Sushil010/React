3a2912f263f5527a346889b9e8e48c451b0200df
Sushil Sharma <sharma.s420823@gmail.com>
2025-07-11 18:44:38 +0545

    addde-

--- stone/src/App.jsx
+++ stone/src/App.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [inputvalue, setinputvalue] = useState("")
+  const Changer=(e)=>{
+    setinputvalue(e.target.value)
+  }
   return (
+    
     <>
-    <div className='bg-[#242424] h-screen w-screen text-white'>
+    
+    <div className='h-screen bg-[#242424] w-screen text-white p-3 gap-2 flex'>  
+      
+      <div className=''>
+        <input
+        className='border border-white p-2 rounded rounded-20px' 
+        onChange={Changer}
+        value={inputvalue}
+        type="text" 
+        placeholder='Enter lists' />
 
+      </div>
       <div>
-        App
+        <button 
+        
+        className='bg-blue-200 p-2 border border-black rounded rounded-20px'>
+          Submit
+        </button>
       </div>
 
     </div>
     
     </>
+    
+    
   )
 }
 
