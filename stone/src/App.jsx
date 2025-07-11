b40e7ecc550a1af6776bc3ce75541537c38fa929
Sushil Sharma <sharma.s420823@gmail.com>
2025-07-11 19:08:02 +0545

    added_idx

--- stone/src/App.jsx
+++ stone/src/App.jsx
@@ -1,10 +1,28 @@
 import React, { useState } from 'react'
+import { div } from 'three/tsl'
 
 const App = () => {
   const [inputvalue, setinputvalue] = useState("")
+  const [handleinput, sethandleinput] = useState([])
+  
+  // For seeing changes that user types in in real time
   const Changer=(e)=>{
     setinputvalue(e.target.value)
+    console.log(inputvalue)
   }
+
+  const Submitter=()=>{
+    if (inputvalue.trim()!=""){
+      sethandleinput([...handleinput,inputvalue])
+      setinputvalue("")
+    }
+    
+  }
+
+  const Handler=()=>{
+    sethandleinput(handleinput.slice(0,-1))
+  }
+
   return (
     
     <>
@@ -22,12 +40,29 @@ const App = () => {
       </div>
       <div>
         <button 
-        
-        className='bg-blue-200 p-2 border border-black rounded rounded-20px'>
+        // onClick={()=>{console.log(" Submit Clicked")}}
+        onClick={Submitter}
+        className='bg-blue-200 p-2 border cursor-pointer border-black rounded rounded-20px'>
           Submit
         </button>
-      </div>
 
+        <button
+          // onClick={()=>{console.log("Delete Clicked")}}
+          onClick={Handler}
+          className='bg-blue-200 cursor-pointer p-2 border border-black rounded rounded-20px'>
+          Delete
+        </button>
+      </div>
+      {handleinput.map((value,index)=>(
+        <div key={index}>
+          {value}
+        </div>  
+        
+      
+      ))}
+    </div>
+    <div>
+      
     </div>
     
     </>
