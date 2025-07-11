2dda1180b7d0ec2a6ad79e40ea8bf92c314d697d
Sushil Sharma <sharma.s420823@gmail.com>
2025-07-11 16:12:54 +0545

    added_ext

--- stone/src/App.jsx
+++ stone/src/App.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <div className='h-screen bg-red-500'>
+      App
+    </div>
+    
+    </>
+    
   )
 }
 
